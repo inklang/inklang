c4fda1946c2c497737c07d1b47c50b22b47c4a0b
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2025-01-17 13:17:57 +0100

    feat: add base for tokens

---
+++ src/token.ts
@@ -0,0 +1,58 @@
+export enum TokenType {
+  // Single-character tokens.
+  LPAREN,
+  RPAREN,
+  LBRACE,
+  RBRACE,
+  LBRACKET,
+  RBRACKET,
+  COLON,
+  COMMA,
+  DOT,
+  MINUS,
+  PLUS,
+  SEMICOLON,
+  SLASH,
+  STAR,
+  AT,
+
+  // 1-2 character tokens.
+  BANG, BANG_EQUAL,
+  EQUAL, EQUAL_EQUAL,
+  GREATER, GREATER_EQUAL,
+  LESS, LESS_EQUAL,
+
+  // Literals.
+  IDENTIFIER,
+  STRING,
+  NUMBER,
+
+  // Keywords.
+  PUBLIC,
+  PRIVATE,
+  RECORD,
+  AND,
+  ELSE,
+  IF,
+  FALSE,
+  TRUE,
+  VAR,
+  WHILE,
+  OR,
+  RETURN,
+
+  EOF
+}
+
+export class Token {
+  public constructor (
+    public readonly type: TokenType,
+    public readonly lexeme: string,
+    public readonly literal: string | null,
+    public readonly line: number
+  ) {}
+
+  public toString (): string {
+    return `${this.type} ${this.lexeme} ${this.literal}`;
+  }
+}
