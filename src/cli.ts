0a78577ddfbb435d014205c1b1c528fd3437d23b
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2025-01-19 23:44:47 +0100

    feat(cli): add `generate` command with `javascript` and `kotlin` available options

--- src/cli.ts
+++ src/cli.ts
@@ -1,4 +1,9 @@
+import fs from "node:fs/promises";
+import path from "node:path";
+
 import { syncKotlin, syncJavaScript, syncRepository } from "./cli/sync";
+import { parse, TranslatorJS, TranslatorKotlin, TranslatorTS } from "./index";
+import { readInkJSON, write } from "./cli/helpers";
 
 async function main () {
   const args = process.argv.slice(2);
@@ -10,6 +15,61 @@ async function main () {
       await syncJavaScript();
       await syncKotlin();
       break;
+    }
+    case "generate": {
+      const entrypoint = path.resolve("./src/main.ink");
+      const code = await fs.readFile(entrypoint, "utf8");
+      const statements = parse(code, entrypoint);
+
+      switch (args.shift()) {
+        case "javascript": {
+          const cjs = new TranslatorJS(statements, "cjs").translate();
+          const mjs = new TranslatorJS(statements, "mjs").translate();
+          const dts = new TranslatorTS(statements).translate();
+
+          await fs.mkdir("generated/javascript", { recursive: true });
+          await write("generated/javascript/index.cjs", cjs);
+          await write("generated/javascript/index.mjs", mjs);
+          await write("generated/javascript/index.d.ts", dts);
+
+          break;
+        }
+        case "kotlin": {
+          const code = new TranslatorKotlin(statements).translate();
+
+          const inkJSON = await readInkJSON();
+          const libSubPath = inkJSON.package.replace(/\./g, "/");
+
+          const libDir = `generated/kotlin/src/main/${libSubPath}`;
+          await fs.mkdir(libDir, { recursive: true });
+
+          await write(`${libDir}/${inkJSON.displayName}.kt`, code);
+          await write("generated/kotlin/build.gradle.kts", `
+plugins {
+  alias(libs.plugins.kotlin.jvm)
+  \`java-library\`
+}
+
+repositories {
+  mavenCentral()
+}
+
+dependencies {
+
+}
+
+java {
+  toolchain {
+    languageVersion = JavaLanguageVersion.of(17)
+  }
+}
+          `.trim());
+          break;
+        }
+        default: {
+          throw new Error(`unknown language: javascript, kotlin`);
+        }
+      }
     }
   }
 }
