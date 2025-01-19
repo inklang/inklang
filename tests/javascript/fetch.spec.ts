dfde8bf846701098d7aa7922cf9d5f94c3d379fe
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2025-01-19 22:54:45 +0100

    chore: write test for fetch in javascript

---
+++ tests/javascript/fetch.spec.ts
@@ -0,0 +1,21 @@
+import { test, expect } from "bun:test";
+import { translate } from "./translate";
+import { codeFormatter } from "../helpers";
+
+test("function declaration", () => {
+  expect(translate(`
+    function do_fetch () -> void {
+      var response = @fetch(
+        url: "https://jsonplaceholder.typicode.com/todos/1"
+      );
+    }
+  `)).toBe(codeFormatter(`
+    const { defaultFetcher } = require("@literate.ink/fetcher");
+
+    const doFetch = (fetcher = defaultFetcher) => {
+      let response = await fetcher({
+        url: "https://jsonplaceholder.typicode.com/todos/1"
+      });
+    };
+  `));
+});
